76acc6e843051ac12e2ea6140e86b72d8f3d408c
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-05-03 01:17:25 -0300

    add auth error and success messages

--- app/(auth)/(pages)/login/(components)/login-form.tsx
+++ app/(auth)/(pages)/login/(components)/login-form.tsx
@@ -1,27 +1,66 @@
 'use client'
 import * as z from "zod"
+import { zodResolver }from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { LoginSchema } from "@/schema";
 import { FormSuccess } from "../../(components)/form-success";
 import { FormError } from "../../(components)/form-error";
+import { useState, useTransition } from "react";
 export const LoginForm = () => {
-    const form = useForm<z.infer<typeof LoginSchema>>();
-    return ( 
-        <form action="" className="flex flex-col gap-4">
+    const [error, setError] = useState<string>('');
+    const [success, setSuccess] = useState<string>('');
+    const [isPending, startTransition] = useTransition();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    }); 
+
+    const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+        setError('');
+        setSuccess('');
+
+        startTransition(() => {
+            setError(" ");
+            setSuccess(" ");
+            console.log(data);
+        })
+    }
+    return (  
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-2">
                 <label htmlFor="emal">Email</label>
-                <input type="email" name="email" id="email" placeholder="ejemplo@email.com" />
+                <input 
+                    {...register('email')}
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    placeholder="ejemplo@email.com" 
+                    disabled={isPending}
+                    /* {errors.email && ( <p className="text-sm text-danger">{errors.email.message}</p> )} */
+                />
             </div>
             <div className="flex flex-col gap-2">
                 <label htmlFor="password">Contraseña</label>
-                <input type="password" name="password" id="password" placeholder="********" />
+                <input 
+                    {...register('password')}
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    placeholder="********" 
+                    disabled={isPending}
+                    /* {errors.password && ( <p className="text-sm text-danger">{errors.password.message}</p> )} */
+                />
 
             </div>
             <div className="flex flex-col gap-2">
-                <FormSuccess message="Login exitoso" />
-                <FormError message="Error en el login" />
                 <button className="w-full bg-primary text-white py-2 rounded-md">Iniciar Sesión</button>
             </div>
+            <FormSuccess message={success} />
+            <FormError message={error} />
         </form> 
     )
 };
--- schema/index.ts
+++ schema/index.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+    email: z.string().email({message: "Email is required"}),
+    password: z.string().min(1, {message: "Password is required"})
 });
