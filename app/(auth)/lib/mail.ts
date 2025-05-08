c295261f89852a6951966168177f3aa2223ab370
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-05-08 07:30:48 -0300

    add email html sended

--- app/(auth)/lib/mail.ts
+++ app/(auth)/lib/mail.ts
@@ -16,44 +16,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
             from: SMTP_EMAIL,
             to: email,
             subject: 'Confirm your email',
-            text: ` <!DOCTYPE html>
-                    <html lang='es'>
-                        <head>
-                            <meta charset='UTF-8'>
-                            <title>Notificación</title>
-                        </head>
-                        <body style='margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4;'>
-                            <table align='center' cellpadding='0' cellspacing='0' width='100%' style='padding: 20px 0;'>
-                                <tr>
-                                    <td align='center'>
-                                        <table width='600' cellpadding='0' cellspacing='0' style='background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 8px rgba(0,0,0,0.1); overflow: hidden;'>
-                                        <tr>
-                                            <td style='background-color: #4CAF50; padding: 20px; color: white; text-align: center;'>
-                                            <h1 style='margin: 0;'>App Name</h1>
-                                            <p style='margin: 5px 0 0;'>Tienes una nueva notificación importante</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style='padding: 30px; color: #333333; text-align: center;'>
-                                            <p style='font-size: 16px;'>Tu solicitud ha sido procesada con éxito.</p>
-                                            <p style='font-size: 16px;'>Confirma tu email y accede a tu cuenta.</p>
-                                            <div style='text-align: center; margin: 30px 0;'>
-                                                <a href=${confirmLink} style='background-color: #4CAF50; color: white; padding: 12px 25px; text-decoration: none; border-radius: 5px; font-weight: bold;'>Confirmar email</a>
-                                            </div>
-                                            <p style='font-size: 14px; color: #777;'>Si no fuiste tú quien solicitó esta acción, por favor ignora este mensaje.</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style='background-color: #eeeeee; padding: 20px; text-align: center; font-size: 12px; color: #888;'>
-                                            © 2025 App Name. Todos los derechos reservados.
-                                            </td>
-                                        </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </table>
-                        </body>
-                    </html> `,       
+            html: `<table align="center" cellpadding="0" cellspacing="0" width="100%" style="padding: 20px 0" width="100%"><tr><td align="center"><table width="600" cellpadding="0" cellspacing="0" style=" background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1); overflow: hidden; " ><tr><td style=" background-color: #4caf50; padding: 20px; color: white; text-align: center; " ><h1 style="margin: 0">App Name</h1><p style="margin: 5px 0 0; color: white; font-size: 16px">Tienes una nueva notificación importante </p></td></tr><tr><td style="padding: 30px; color: #333333; text-align: center"><p style="font-size: 16px">Tu solicitud ha sido procesada con éxito. </p><p style="font-size: 16px">Confirma tu email y accede a tu cuenta. </p><div style="text-align: center; margin: 30px 0"><a href="${confirmLink}" style=" background-color: #4caf50; color: white; padding: 12px 25px; text-decoration: none; border-radius: 5px; font-weight: bold; " >Confirmar email</a ></div><p style="font-size: 14px; color: #777">Si no fuiste tú quien solicitó esta acción, por favor ignora este mensaje. </p></td></tr><tr><td style=" background-color: #eeeeee; padding: 20px; text-align: center; font-size: 12px; color: #888; " >© 2025 App Name. Todos los derechos reservados. </td></tr></table></td></tr></table>`,
         });
     }
     catch(err){
