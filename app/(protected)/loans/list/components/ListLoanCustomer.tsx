e639eac2c12a72d9dc245aa6829d600c262ca2cb
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-05-15 05:41:40 -0300

    add list loan view per customer

--- app/(protected)/loans/list/components/ListLoanCustomer.tsx
+++ app/(protected)/loans/list/components/ListLoanCustomer.tsx
@@ -34,16 +34,21 @@ const ListLoanCustomer = () => {
     fetchData();
     
   },[customerId])
-
-  if(!listLoan){
+  
+  if(listLoan?.length == 0){
     return (
       <div className='flex gap-4 mt-8 items-center justify-center text-xl text-secondary/50'><FaMoneyBillTransfer className="text-2xl"/> Aun no registra prestamos</div>
     )
   }
+
   return (
     <div className='flex flex-col gap-4 mt-4'>
         {listLoan?.map((loan, index) => (
-          <Link key={index} href={'/loans/quotas'} className='flex gap-2 items-center justify-between px-4 py-2 bg-white mx-2 rounded-lg shadow-md border-l-4 border-l-danger'> 
+          <Link 
+            key={index} 
+            href={'/loans/quotas'} 
+            className={`flex gap-2 items-center justify-between px-4 py-2 bg-white mx-2 rounded-lg shadow-md border-l-4 ${loan.statusId == 1 ? 'border-l-warning' : loan.statusId == 2 ? 'border-l-success' : 'border-l-danger'}`}
+          > 
               <div className='flex flex-col w-full'>
                 <div className='flex items-center justify-between w-full pr-4'>
                   <p className='text-xl font-semibold'>${loan.amount}</p>
