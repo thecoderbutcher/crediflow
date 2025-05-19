79fad278ca260d3423339ec60d23c413eeb0bb18
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-05-18 21:55:22 -0300

    apply asyncrous sentenses for optimization response on installments view

--- app/(protected)/loans/view/[id]/page.tsx
+++ app/(protected)/loans/view/[id]/page.tsx
@@ -1,47 +1,29 @@
-'use client';
 import Header from '@/app/(protected)/components/Header';
 import LoanInfo from '../components/LoanInfo';
 import FilterQuota from '@/app/(protected)/installments/components/FilterInstallments';
 import ListInstallments from '@/app/(protected)/installments/components/ListInstallments';
 import { viewInstallments } from '@/app/(protected)/installments/action/installments';
-import { Loan, Installments } from '@prisma/client';
 import { viewLoan } from '../action/view';
-import { useCustomersStore } from '@/app/(protected)/customers/store/customerStore';
-import { useEffect, useState } from 'react';
 interface pageProps {
   params: { id: string };
 }
 
-const Page = ({ params }: pageProps) => {
-  const { customerId, customerName } = useCustomersStore();
-  const [loan, setLoan] = useState<Loan>();
-  const [installments, setInstallments] = useState<Installments[]>([]);
-
-  useEffect(() => {
-    const getAsyncData = async () => {
-      const loanId = Number((await params).id);
-
-      const loanData = await viewLoan(loanId);
-      if (loanData) setLoan(loanData);
-      else setLoan(undefined);
-
-      const installmentsData = await viewInstallments(loanId);
-      setInstallments(installmentsData);
-    };
-    getAsyncData();
-  }, [params, setLoan]);
+const page = async ({ params }: pageProps) => {
+  const { id } = await params;
+  const loans = await viewLoan(Number(id));
+  const installments = await viewInstallments(Number(id));
 
   return (
     <div className="flex flex-col h-screen w-full">
       <Header
-        title={`Prestamo: ${customerName}`}
-        url={`/customers/view/${customerId}`}
+        title={`Prestamo: ${loans?.customer?.firstName} ${loans?.customer?.lastName}`}
+        url={`/customers/view/${loans?.customerId}`}
       />
-      {loan && <LoanInfo loan={loan} />}
+      {loans && <LoanInfo loan={loans} />}
       <FilterQuota />
       {installments && <ListInstallments installments={installments} />}
     </div>
   );
 };
 
-export default Page;
+export default page;
--- app/(protected)/loans/view/action/view.ts
+++ app/(protected)/loans/view/action/view.ts
@@ -2,5 +2,5 @@
 import { db } from "@/lib/db";
 
 export const viewLoan = async (loanId: number) => {
-  return await db.loan.findUnique({ where: { id: loanId }, include: { paymentFrequency: true, loanType: true } });
+  return await db.loan.findUnique({ where: { id: loanId }, include: { paymentFrequency: true, loanType: true, customer: true } });
 }
